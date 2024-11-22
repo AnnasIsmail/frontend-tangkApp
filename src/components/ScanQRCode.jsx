8103c9861b3058a567ed63913cdbbb0df229c68d
Anans PC <annasismailmuhammad@gmail.com>
2024-11-23 02:13:58 +0700

    test qr code

--- src/components/ScanQRCode.jsx
+++ src/components/ScanQRCode.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import QrScanner from 'react-qr-scanner';  // Import QR scanner
-import { useEffect } from 'react';
 
 const ScanQRCode = () => {
   const [result, setResult] = useState('');
@@ -17,33 +16,20 @@ const ScanQRCode = () => {
     console.error(err); // Menangani error jika ada
   };
 
-  const checkCameraDevices = () => {
-    navigator.mediaDevices.enumerateDevices()
-      .then(devices => {
-        const videoDevices = devices.filter(device => device.kind === 'videoinput');
-        console.log(videoDevices); // Menampilkan semua perangkat kamera
-        // Lihat apakah ada kamera belakang
-        const backCamera = videoDevices.find(device => device.label.includes('back'));
-        console.log(backCamera);
-      })
-      .catch(error => console.error('Error accessing devices:', error));
-  };
-  
-  useEffect(() => {
-    checkCameraDevices();
-  }, []);
-
   return (
     <div>
       <h1>Scan QR Code</h1>
       {scanning ? (
         <QrScanner
-          delay={300}
-          style={{ width: '100%' }}
-          onScan={handleScan}
-          onError={handleError}
-          facingMode={{ exact: "environment" }}  // Pastikan menggunakan objek seperti ini
-        />
+  delay={300}
+  style={{ width: '100%' }}
+  onScan={handleScan}
+  onError={handleError}
+  constraints={{
+    facingMode: { exact: 'environment' }, // Gunakan objek untuk lebih eksplisit
+  }}
+/>
+
       ) : (
         <div>
           <h2>Scan Berhasil!</h2>
